b4f78395a4f8299fc03d1312b02c3ba0b0da17a7
Mohan Kumar <tomhollderdgl53@gmail.com>
2026-06-17 16:58:44 +0530

    Add Dockerfile and .dockerignore; implement Express server with static file serving

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,16 @@
 import dns from "node:dns";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
 dns.setServers(["8.8.8.8", "1.1.1.1"]);
 
 import express from "express";
 import cors from "cors";
+
 import "dotenv/config";
+
+import fs from "fs";
+import path from "node:path";
 
 import { connectDB } from "./lib/db.js";
 import { clerkMiddleware } from '@clerk/express'
@@ -14,6 +20,8 @@ const app = express();
 const PORT = process.env.PORT;
 const FRONTEND_URL = process.env.FRONTEND_URL;
 
+const publicDir = path.join(process.cwd(),"public");
+
 app.use(express.json);
 app.use(cors({origin:FRONTEND_URL,credentials:true}));
 app.use(clerkMiddleware())
@@ -22,7 +30,24 @@ app.get("/health",(req,res)=>{
     res.status(200).json({ok:true});
 });
 
+if(fs.existsSync(publicDir)){
+  app.use(express.static(publicDir));
+
+  app.get("/{*any",(req,res,next)=>{
+    res.sendFile(path.join(publicDir, "index.html"), (err) => next(err));
+  })
+}
+
+if (process.env.NODE_ENV === "production") {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = path.dirname(__filename);
+  app.use(express.static(path.join(__dirname, "..", "public")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+  });
+}
+
 app.listen( PORT, ()=>{ 
     connectDB();
-    console.log("Server is stared and running on port", PORT)
+    console.log("Server is started and running on port", PORT)
 });
