1be0f23f79eed5f4828fae7a6bb4d82b7fd4c8df
Mohan Kumar <tomhollderdgl53@gmail.com>
2026-07-20 19:47:32 +0530

    Add routing and basic page structure with Auth and Chat pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,27 @@
-import { Show, SignInButton, SignUpButton, UserButton } from '@clerk/react'
-import { Button } from '@heroui/react';
+import { ThemeProvider } from './context/ThemeContext';
+import { WallpaperProvider } from './context/WallpaperContext';
+import { Navigate, Route, Routes } from 'react-router';
+import ChatPage from './pages/ChatPage';
+import AuthPage from './pages/AuthPage';
+import { useAuth } from '@clerk/react';
 
 function App() {
+
+  const { isSignedIn, isLoaded } = useAuth();
+  
+  // todo: make this a better component
+  if (!isLoaded) return <p>loading...</p>;
+
   return (
-    <>
-      <h1 className='text-4xl text-red-500 bg-blue-400'>MY APP</h1>
-      <Button>
-        My Button
-      </Button>
-      <header>
-        <Show when="signed-out">
-          <SignInButton mode='modal' />
-          <SignUpButton mode='modal' />
-        </Show>
-        <Show when="signed-in">
-          <UserButton />
-        </Show>
-      </header>
-    </>
+    <ThemeProvider>
+      <WallpaperProvider>
+        <Routes>
+          <Route path="/" element={ isSignedIn ? <ChatPage /> : <Navigate to={"/auth"} replace /> } />
+          <Route path="/auth" element={ !isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace /> } />
+        </Routes>
+      </WallpaperProvider>
+    </ThemeProvider>
   )
 }
 
-export default App
+export default App;
